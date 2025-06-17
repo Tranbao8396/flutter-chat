074f785618da95895f8a1e39bfe8af4adef3d0ff
Tran Ngoc Bao <baotn@icd-vn.com>
2025-06-17 10:07:48 +0700

    wip

--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -8,7 +8,8 @@ class SignupScreen extends StatelessWidget {
   final _formKey = GlobalKey<FormState>();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-  final TextEditingController confirmPasswordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
   final TextEditingController nameController = TextEditingController();
 
   SignupScreen({super.key});
@@ -18,232 +19,234 @@ class SignupScreen extends StatelessWidget {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Theme.of(context).primaryColor,
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              const SizedBox(height: 20.0),
-              Text(
-                'Sign Up',
-                style: GoogleFonts.poppins(
-                  fontSize: 36.0,
-                  fontWeight: FontWeight.bold,
-                  color: Theme.of(context).colorScheme.secondary,
-                ),
-              ),
-              const SizedBox(height: 20.0),
-              TextFormField(
-                style: GoogleFonts.poppins(
-                  color: Theme.of(context).colorScheme.secondary,
-                ),
-                controller: nameController,
-                cursorColor: Colors.white,
-                decoration: InputDecoration(
-                  labelText: 'Name',
-                  labelStyle: const TextStyle(color: Colors.grey),
-                  floatingLabelStyle: TextStyle(
-                    color: Theme.of(context).colorScheme.primaryContainer,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const SizedBox(height: 20.0),
+                Text(
+                  'Sign Up',
+                  style: GoogleFonts.poppins(
+                    fontSize: 36.0,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.secondary,
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
+                ),
+                const SizedBox(height: 20.0),
+                TextFormField(
+                  style: GoogleFonts.poppins(
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+                  controller: nameController,
+                  cursorColor: Colors.white,
+                  decoration: InputDecoration(
+                    labelText: 'Name',
+                    labelStyle: const TextStyle(color: Colors.grey),
+                    floatingLabelStyle: TextStyle(
                       color: Theme.of(context).colorScheme.primaryContainer,
                     ),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Theme.of(context).colorScheme.secondary,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Theme.of(context).colorScheme.primaryContainer,
+                      ),
                     ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+                    ),
+                    border: const OutlineInputBorder(),
                   ),
-                  border: const OutlineInputBorder(),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter an name';
+                    }
+                    return null;
+                  },
                 ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter an name';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 16.0),
-              TextFormField(
-                style: GoogleFonts.poppins(
-                  color: Theme.of(context).colorScheme.secondary,
-                ),
-                controller: emailController,
-                cursorColor: Colors.white,
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  labelStyle: const TextStyle(color: Colors.grey),
-                  floatingLabelStyle: TextStyle(
-                    color: Theme.of(context).colorScheme.primaryContainer,
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  style: GoogleFonts.poppins(
+                    color: Theme.of(context).colorScheme.secondary,
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
+                  controller: emailController,
+                  cursorColor: Colors.white,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    labelStyle: const TextStyle(color: Colors.grey),
+                    floatingLabelStyle: TextStyle(
                       color: Theme.of(context).colorScheme.primaryContainer,
                     ),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Theme.of(context).colorScheme.secondary,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Theme.of(context).colorScheme.primaryContainer,
+                      ),
                     ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+                    ),
+                    border: const OutlineInputBorder(),
                   ),
-                  border: const OutlineInputBorder(),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter an email';
+                    }
+                    if (!RegExp(r'^[^@]+@[^@]+\.[^@]+$').hasMatch(value)) {
+                      return 'Please enter a valid email address';
+                    }
+                    return null;
+                  },
                 ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter an email';
-                  }
-                  if (!RegExp(r'^[^@]+@[^@]+\.[^@]+$').hasMatch(value)) {
-                    return 'Please enter a valid email address';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 16.0),
-              TextFormField(
-                controller: passwordController,
-                obscureText: true,
-                cursorColor: Colors.white,
-                style: GoogleFonts.poppins(
-                  color: Theme.of(context).colorScheme.secondary,
-                ),
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  labelStyle: const TextStyle(color: Colors.grey),
-                  floatingLabelStyle: TextStyle(
-                    color: Theme.of(context).colorScheme.primaryContainer,
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  controller: passwordController,
+                  obscureText: true,
+                  cursorColor: Colors.white,
+                  style: GoogleFonts.poppins(
+                    color: Theme.of(context).colorScheme.secondary,
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: const TextStyle(color: Colors.grey),
+                    floatingLabelStyle: TextStyle(
                       color: Theme.of(context).colorScheme.primaryContainer,
                     ),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Theme.of(context).colorScheme.secondary,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Theme.of(context).colorScheme.primaryContainer,
+                      ),
                     ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+                    ),
+                    border: const OutlineInputBorder(),
                   ),
-                  border: const OutlineInputBorder(),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a password';
+                    }
+                    if (value.length < 6) {
+                      return 'Password must be at least 6 characters long';
+                    }
+                    return null;
+                  },
                 ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a password';
-                  }
-                  if (value.length < 6) {
-                    return 'Password must be at least 6 characters long';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 20.0),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () async {
-                    if (_formKey.currentState?.validate() ?? false) {
-                      // Perform sign-up action
-                      try {
-                        final UserCredential? user =
-                            await Authentication.signUpWithEmailAndPassword(
-                          emailController.text,
-                          passwordController.text,
-                          nameController.text,
-                        );
-                        if (user != null && context.mounted) {
+                const SizedBox(height: 20.0),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      if (_formKey.currentState?.validate() ?? false) {
+                        // Perform sign-up action
+                        try {
+                          final UserCredential? user =
+                              await Authentication.signUpWithEmailAndPassword(
+                                emailController.text,
+                                passwordController.text,
+                                nameController.text,
+                              );
+                          if (user != null && context.mounted) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                elevation: 5.0,
+                                behavior: SnackBarBehavior.floating,
+                                padding: const EdgeInsets.all(10.0),
+                                content: Text(
+                                  'Sign up successful!',
+                                  style: GoogleFonts.poppins(
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: Theme.of(context).primaryColor,
+                                  ),
+                                ),
+                                backgroundColor: Theme.of(
+                                  context,
+                                ).colorScheme.secondary,
+                              ),
+                            );
+                            Navigator.of(context).pop();
+                          }
+                        } catch (error) {
+                          Logger().e(error);
+                          if (!context.mounted) return;
                           ScaffoldMessenger.of(context).showSnackBar(
                             SnackBar(
                               elevation: 5.0,
                               behavior: SnackBarBehavior.floating,
                               padding: const EdgeInsets.all(10.0),
                               content: Text(
-                                'Sign up successful!',
+                                'Something went wrong. Please try again.',
                                 style: GoogleFonts.poppins(
                                   fontSize: 16.0,
-                                  fontWeight: FontWeight.bold,
-                                  color: Theme.of(context).primaryColor,
+                                  color: Colors.red,
                                 ),
                               ),
-                              backgroundColor:
-                                  Theme.of(context).colorScheme.secondary,
+                              backgroundColor: Theme.of(
+                                context,
+                              ).colorScheme.secondary,
                             ),
                           );
-                          Navigator.of(context).pop();
                         }
-                      } catch (error) {
-                        Logger().e(error);
-                        if (!context.mounted) return;
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            elevation: 5.0,
-                            behavior: SnackBarBehavior.floating,
-                            padding: const EdgeInsets.all(10.0),
-                            content: Text(
-                              'Something went wrong. Please try again.',
-                              style: GoogleFonts.poppins(
-                                fontSize: 16.0,
-                                color: Colors.red,
-                              ),
-                            ),
-                            backgroundColor:
-                                Theme.of(context).colorScheme.secondary,
-                          ),
-                        );
                       }
-                    }
-                  },
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      backgroundColor: Theme.of(context).colorScheme.secondary,
                     ),
-                    backgroundColor: Theme.of(context).colorScheme.secondary,
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Text(
-                      'Sign Up',
-                      style: GoogleFonts.poppins(
-                        fontSize: 16.0,
-                        color: Theme.of(context).primaryColor,
-                        fontWeight: FontWeight.bold,
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Text(
+                        'Sign Up',
+                        style: GoogleFonts.poppins(
+                          fontSize: 16.0,
+                          color: Theme.of(context).primaryColor,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 10.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    "Already have an Account?",
-                    style: GoogleFonts.poppins(
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.w500,
-                      color: Theme.of(context).colorScheme.secondary,
-                    ),
-                  ),
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      padding: EdgeInsets.zero,
-                    ),
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/login');
-                    },
-                    child: Text(
-                      "Login",
+                const SizedBox(height: 10.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Already have an Account?",
                       style: GoogleFonts.poppins(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).colorScheme.primaryContainer,
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.w500,
+                        color: Theme.of(context).colorScheme.secondary,
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                    TextButton(
+                      style: TextButton.styleFrom(padding: EdgeInsets.zero),
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/login');
+                      },
+                      child: Text(
+                        "Login",
+                        style: GoogleFonts.poppins(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).colorScheme.primaryContainer,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
