0b477063287fc0530f79e48be62203f0ab6181c5
Tran Ngoc Bao <baotn@icd-vn.com>
2025-06-23 10:45:54 +0700

    wip

--- lib/services/message_service.dart
+++ lib/services/message_service.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:logger/web.dart';
 import 'package:multi_message/models/message_model.dart';
 
 class MessageService extends ChangeNotifier {
@@ -14,6 +18,11 @@ class MessageService extends ChangeNotifier {
     final Timestamp timestamp = Timestamp.now();
     final String currentUserId = _firebaseAuth.currentUser!.uid;
     final String currentUserEmail = _firebaseAuth.currentUser!.email!;
+    final String currentUserName = await _fireStore
+        .collection('users')
+        .doc(currentUserId)
+        .get()
+        .then((doc) => doc.data()?['name'] ?? '');
 
     List<String> chatId = [currentUserId, email];
     chatId.sort();
@@ -30,6 +39,27 @@ class MessageService extends ChangeNotifier {
             senderEmail: currentUserEmail,
           ).toMap(),
         );
+
+    // Gửi thông báo đến người nhận
+    final receiverUserToken = await _fireStore
+      .collection('users')
+      .doc(email)
+      .get()
+      .then((doc) => doc.data()?['fcm_token'] ?? '');
+
+    Logger().i('Bắt Đầu gửi thông báo đến người dùng: $email');
+    final push = await post(
+      Uri.parse('http://10.0.2.2:3000/send-notification'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'token': receiverUserToken,
+        'title': 'Tin nhắn mới từ $currentUserName',
+        'body': message,
+      }),
+    );
+
+    Logger().i('Đã gửi thông báo đến người dùng: $email');
+    Logger().i(push);
   }
 
   Stream<QuerySnapshot> getMessages({
